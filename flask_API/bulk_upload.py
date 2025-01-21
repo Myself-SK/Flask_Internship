54743e33c4f42ef1fa40532c0e0dbe9694babdbd
Myself-SK <mail.shivakumar03@gmail.com>
2025-01-21 10:07:14 +0530

    Updated Bulk Upload with actions

--- flask_API/bulk_upload.py
+++ flask_API/bulk_upload.py
@@ -19,6 +19,58 @@ def addEmployee(data):
     except:
         return False
 
+def updateEmployee(data):
+    try:
+        q = f"update employee set name='{data['name']}', designation='{data['designation']}' where email='{data['email']}' and empId = '{data['empId']}'"
+        cur.execute(q)
+        conn.commit()
+        return True
+    except:
+        return False
+
+def deleteEmployee(data):
+    try:
+        q = f"delete from employee where email='{data['email']}' and empId = '{data['empId']}'"
+        cur.execute(q)
+        conn.commit()
+        return True
+    except:
+        return False
+
+def checkExist(data):
+    try:
+        q = f"select * from employee where email = '{data['email']}'"
+        cur.execute(q)
+        res = cur.fetchall()
+        if len(res)>0:
+            return True
+        else:
+            return False
+    except:
+        pass
+
+def driverCode(data):
+    try:
+
+        if data['Action'] == "Add":
+            if checkExist(data):
+                updateEmployee(data)
+            else:
+                addEmployee(data)
+
+        if data['Action'] == "Update":
+            if not checkExist(data):
+                return False
+            else:
+                updateEmployee(data)
+        if data['Action'] == "Delete":
+            if checkExist(data):
+                deleteEmployee(data)
+            else:
+                return False
+    except:
+        pass
+ 
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
@@ -29,15 +81,24 @@ def index():
         data = pd.read_excel(f.filename)
         print(data.head())
         d = data.to_dict("records")
-        count = 0
+        updatedCount = 0
+        addedCount = 0
+        failedCount = 0
         errItems = []
         for i in d:
-            if addEmployee(i):
-                count+=1
-            else:
-                errItems(i)
-        return jsonify({"message":"Data Added Successfully","Total Updated Count":count,"Total Failed Employees":errItems,"Failed Employee Count":len(errItems)})
+            driverCode(i)
+        return jsonify({
+            "message":"Data Added Successfully",
+            # "Total Added Count":addedCount,
+            # "Total Updated Count":updatedCount,
+            # "Total Failed Count":failedCount,
+            # "Total Failed Employees":errItems,
+            # "Failed Employee Count":len(errItems)
+            })
     return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# delete from <table> where <col>=<value>z
